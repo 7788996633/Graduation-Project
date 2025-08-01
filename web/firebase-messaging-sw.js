f1370e5a0658be3f786c1a5d3859a047727e720d
farezaalhallak <farezaalhallak28@gmail.com>
2025-08-01 14:38:51 +0300

    initial commit

---
+++ web/firebase-messaging-sw.js
@@ -0,0 +1,20 @@
+// Please see this file for the latest firebase-js-sdk version:
+// https://github.com/firebase/flutterfire/blob/main/packages/firebase_core/firebase_core_web/lib/src/firebase_sdk_version.dart
+importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+       apiKey: 'AIzaSyDA8w2eKNM_7NHTxKEPonqXhdjRDl5YYUY',
+       appId: '1:34322017082:web:fb63ba4cb3be44b8397e03',
+       messagingSenderId: '34322017082',
+       projectId: 'lawcompanyapp',
+       authDomain: 'lawcompanyapp.firebaseapp.com',
+       storageBucket: 'sham-al-maaref.firebasestorage.app',
+});
+
+const messaging = firebase.messaging();
+
+// Optional:
+messaging.onBackgroundMessage((message) => {
+  console.log("onBackgroundMessage", message);
+});
